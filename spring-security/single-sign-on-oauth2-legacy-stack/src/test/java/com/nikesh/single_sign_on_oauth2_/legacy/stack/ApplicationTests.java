d86df0f9b3bfbba9dac92888ec984afbe8f2fa00
Nikesh-TT <73995728+Nikesh-TT@users.noreply.github.com>
2020-11-06 18:27:39 +0530

    Added project Single-Sign-On-OAuth2-(legacy stack)

---
+++ spring-security/single-sign-on-oauth2-legacy-stack/src/test/java/com/nikesh/single_sign_on_oauth2_/legacy/stack/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikesh.single_sign_on_oauth2_.legacy.stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
