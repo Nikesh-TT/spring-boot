dd4e93738bd4380b7c32e90075a3f82dd85823eb
Nikesh-TT <73995728+Nikesh-TT@users.noreply.github.com>
2020-11-06 21:29:35 +0530

    Added single-sign-on-oauth2-legacy_stack

---
+++ spring-security/spring-security/single-sign-on-oauth2-legacy_stack/src/main/java/com/nikesh/single_sign_on_oauth2_legacy_stack/SingleSignOnOauth2LegacyStackApplication.java
@@ -0,0 +1,13 @@
+package com.nikesh.single_sign_on_oauth2_legacy_stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSignOnOauth2LegacyStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleSignOnOauth2LegacyStackApplication.class, args);
+	}
+
+}
